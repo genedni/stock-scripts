4b54a828ac7264a4091ca46d307bce785f022830
genedni <32857016+genedni@users.noreply.github.com>
2020-10-16 20:37:34 -0500

    Add files via upload

---
+++ ydump-sp400.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import csv
+from pandas_datareader import data as pdr
+import yfinance as yf
+
+yf.pdr_override()
+outdir = 'test'
+
+with open('sp400.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        line_count += 1
+        print(f'Pulling data for {row[0]}')
+        data = pdr.get_data_yahoo(row[0])
+        data.to_csv(f'{outdir}/{row[0]}.csv')
+    print(f'Processed {line_count} lines.')
